73406e3cf853a8895e3301be67a34885af97b2bc
Nelson <nelsonobuya19@gmail.com>
2024-01-15 09:52:42 +0300

    Added the HTML and styling in Bootstrap for the content with a ternary operator on the input

---
+++ search-box.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- //bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Search box</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-header text-center">
+                        <h4>Seachbox with filter</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-7">
+
+                                <form action="" method="GET">
+                                <div class="input-group mb-3">
+                                    <input <?= isset($_GET['search']) ? $_GET['search'] : ''?> type="text" class="form-control" placeholder="Search data" name="search" value="">
+                                    <button type="submit" class="btn btn-primary ">Search</button>
+                                   
+                                </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
