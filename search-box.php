4f06737fc66a12f1a355985cfd336a9b6cbbe270
Nelson <nelsonobuya19@gmail.com>
2024-01-15 14:26:52 +0300

    Adding the query to search and excecuting it

--- search-box.php
+++ search-box.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- //bootstrap css -->
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <title>Search box</title>
 </head>
 
@@ -17,18 +18,16 @@
             <div class="col-md-12">
                 <div class="card mt-4">
                     <div class="card-header text-center">
-                        <h4>Seachbox with filter</h4>
+                        <h4>Search box with filter</h4>
                     </div>
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-7">
-
                                 <form action="" method="GET">
-                                <div class="input-group mb-3">
-                                    <input <?= isset($_GET['search']) ? $_GET['search'] : ''?> type="text" class="form-control" placeholder="Search data" name="search" value="">
-                                    <button type="submit" class="btn btn-primary ">Search</button>
-                                   
-                                </div>
+                                    <div class="input-group mb-3">
+                                        <input type="text" class="form-control" placeholder="Search data" name="search" value="<?= isset($_GET['search']) ? $_GET['search'] : '' ?>">
+                                        <button type="submit" class="btn btn-primary">Search</button>
+                                    </div>
                                 </form>
                             </div>
                         </div>
@@ -39,25 +38,36 @@
             <div class="col-md-12">
                 <div class="card mt-4">
                     <div class="card-body">
-                        <table class="table table-bordered ">
+                        <table class="table table-bordered">
                             <thead>
                                 <tr>
                                     <th>ID</th>
                                     <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>email</th>
+                                    <th>Email</th>
                                 </tr>
                             </thead>
-
                             <tbody>
                                 <?php
                                 require("./mysqli_connect.php");
-                                
-                                ?>
 
-                                <tr>
-                                    <td></td>
-                                </tr>
+                                if (isset($_GET['search'])) {
+                                    $filtervalues = '%' . $_GET['search'] . '%';
+                                    $select_query = "SELECT id, first_name, email FROM users WHERE CONCAT(first_name, email) LIKE '$filtervalues'";
+                                    $excecute = mysqli_query($dbcon, $select_query);
+
+                                    if ($excecute && mysqli_num_rows($excecute) > 0) {
+                                        while ($row = mysqli_fetch_assoc($excecute)) {
+                                            echo "<tr>";
+                                            echo "<td>{$row['id']}</td>";
+                                            echo "<td>{$row['first_name']}</td>";
+                                            echo "<td>{$row['email']}</td>";
+                                            echo "</tr>";
+                                        }
+                                    } else {
+                                        echo "<tr><td colspan='3'>No Record Found</td></tr>";
+                                    }
+                                }
+                                ?>
                             </tbody>
                         </table>
                     </div>
